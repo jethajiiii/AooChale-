da0c461ebc7ae8db9e425108e6cadd55b356e72b
Mukul Bhardwaj <mukulbhardwaj102@gmail.com>
2025-09-21 17:41:01 +0530

    first backend commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
+const connectToDB = require('./db/db');
+const userRoutes = require('./routes/user.routes');
+const captainRoutes = require('./routes/captain.routes');
+const cookieParser = require('cookie-parser');
+
+connectToDB();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+}); 
+
+app.use('/users', userRoutes);
+app.use('/captains', captainRoutes); 
+
+
+module.exports = app;
